11966ea82e656e4120aef6c95d6191d73fcb078b
root <zephyne@github.com>
2015-02-07 16:37:19 -0500

    add proxy

---
+++ SimpleProxy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+from twisted.web import proxy,http
+from twisted.internet import reactor
+
+class ProxyFactory(http.HTTPFactory):
+	def buildProtocol(self,addr):
+		return proxy.Proxy()
+
+def start(port):
+	reactor.listenTCP(int(port),ProxyFactory())
+	#reactor.run(installSignalHandlers=False)
+	print "Proxy Server is running on port: "+str(port)
+
+
